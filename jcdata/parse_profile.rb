444568a21b561f2cf7761c164fb18833be69885e
atlas7 <johnnychan0302@gmail.com>
2016-03-12 01:19:50 +0000

    tidy up

--- jcdata/parse_profile.rb
+++ jcdata/parse_profile.rb
@@ -6,4 +6,21 @@
 @profile = Profile.last
 @profile.socials.kinds
 @profile.socials.all.where(kind: 2) # => gives you the user facebook account
-@profile.socials.all.where(kind: :facebook) # => gives you nothing. Why? Apparently only works in Rails or above.
+@profile.socials.all.where(kind: :facebook) # => Apparently only works in Rails 5 or above.
+
+@profile.socials.all.where(kind: [2, 3]) # => gives you the user facebook and linked_in account
+@profile.socials.all.where(kind: [:facebook, :linked_in]) # => Apparently only works in Rails 5 or above.
+
+# In Rails 4...
+
+# These two queries are equivalent (select only the user's facebook account):
+
+@profile.socials.all.where(kind: 2)
+
+@profile.socials.all.where(kind: @profile.socials.kinds.keys.find_index("facebook"))
+
+# These two queries are equivalent (select only the user's facebook and linked_in account):
+
+@profile.socials.all.where(kind: [2, 3])
+
+@profile.socials.all.where(kind: [@profile.socials.kinds.keys.find_index("facebook"), @profile.socials.kinds.keys.find_index("linked_in")])
