fae6800df94661cbeb401db7f60f5a486ff29bc1
atlas7 <johnnychan0302@gmail.com>
2016-03-12 00:54:25 +0000

    correct README typo

--- jcdata/parse_profile.rb
+++ jcdata/parse_profile.rb
@@ -3,7 +3,7 @@
 @profile.last_name = parameter[:profile][:last_name]
 @profile.socials_attributes = parameter[:profile][:socials_attributes]
 @profile.save
-@profile.Profile.last
+@profile = Profile.last
 @profile.socials.kinds
 @profile.socials.all.where(kind: 2) # => gives you the user facebook account
-@profile.socials.all.where(kind: :facebook) # => gives you nothing. Why?
+@profile.socials.all.where(kind: :facebook) # => gives you nothing. Why? Apparently only works in Rails or above.
